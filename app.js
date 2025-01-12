183e71dde29578c945f7e78ce339ef9b01d12606
Alex-Alessi <alex.alessi246@gmail.com>
2025-01-12 20:46:40 +0100

    setup

---
+++ app.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+//# CONFIG EXPRESS
+const express = require("express");
+const app = express();
+const { APP_HOST, APP_PORT } = process.env;
+
+//# MIDDLEWARES
+app.use(express.json());
+app.use(express.static("public"));
+
+//# REGISTERING ROUTES
+const moviesRouter = require("./routers/moviesRouter");
+app.use("/movies", moviesRouter);
+
+//# MANAGE ERRORS
+const notFound = require("./middlewares/notFound");
+const errorsHandler = require("./middlewares/errorsHandler");
+app.use(notFound);
+app.use(errorsHandler);
+
+//#SERVER LISTENING
+app.listen(APP_PORT, () => {
+  console.log(`App listening on ${APP_HOST}:${APP_PORT}`);
+});
